6528ed038da17b2e183000909fb9b5c7aa65afc3
Pierre Convert <pierreconvert@macbook-pro-6.home>
2023-10-09 16:31:58 +0200

    build udpated

--- sources/module-info.java
+++ sources/module-info.java
@@ -17,10 +17,6 @@ module com.s8.io.bohr.atom {
 	exports com.s8.io.bohr.atom.serial;
 
 
-
-	
-	
-
-	requires transitive com.s8.io.bytes;
 	requires transitive com.s8.api;
+	requires transitive com.s8.io.bytes;
 }
